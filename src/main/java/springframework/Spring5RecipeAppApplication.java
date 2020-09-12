9f45810a20500c6c1fb898f237ed63ae74cc75a6
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-12 10:52:27 +0200

    Notes and Recipe entities created. Closes #1

--- src/main/java/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/springframework/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package springframework.spring5recipeapp;
+package springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/springframework/controllers/IndexController.java
+++ src/main/java/springframework/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package springframework.spring5recipeapp.controllers;
+package springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
