84edc9a47c74cfa5d0ffa583cb124ff0e99414c2
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-13 19:40:41 +0200

    Created RecipeService and Impl, created DataLoader and two recipes for testing purposes, updated IndexController and display Recipe List at index.html.

--- src/main/java/springframework/controllers/IndexController.java
+++ src/main/java/springframework/controllers/IndexController.java
@@ -1,13 +1,27 @@
 package springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.services.RecipeService;
+
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"","/","index","index.html"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
--- src/main/java/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,7 +1,12 @@
 package springframework.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import springframework.domain.Category;
 import springframework.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
