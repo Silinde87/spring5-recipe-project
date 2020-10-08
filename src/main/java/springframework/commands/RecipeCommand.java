f5e6eb4f7f99d6b9d31a79d18ff5bd8999c0807a
Silinde87 <paurodriguezmolina@gmail.com>
2020-10-08 11:33:03 +0200

    Annotation Validations added to RecipeCommand. Closes #36

--- src/main/java/springframework/commands/RecipeCommand.java
+++ src/main/java/springframework/commands/RecipeCommand.java
@@ -3,11 +3,16 @@ package springframework.commands;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 import springframework.domain.Category;
 import springframework.domain.Difficulty;
 import springframework.domain.Ingredient;
 import springframework.domain.Notes;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,13 +21,30 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private NotesCommand notes;
--- src/main/java/springframework/controllers/RecipeController.java
+++ src/main/java/springframework/controllers/RecipeController.java
@@ -3,12 +3,15 @@ package springframework.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import springframework.commands.RecipeCommand;
 import springframework.exceptions.NotFoundException;
 import springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -41,7 +44,15 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+            return "recipe/recipeform";
+        }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
