2dc595c802f7fd126d00840657c28c0f2565b182
Silinde87 <paurodriguezmolina@gmail.com>
2020-10-08 10:18:20 +0200

    404 html added and controller modified. Closes #32

--- src/main/java/springframework/controllers/RecipeController.java
+++ src/main/java/springframework/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package springframework.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import springframework.commands.RecipeCommand;
+import springframework.exceptions.NotFoundException;
 import springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -50,6 +53,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
 
